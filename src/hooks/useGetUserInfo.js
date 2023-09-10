9a6e23c3fbe5119919d49ae441c08a9834635f0d
Himanshu Soni <himanshusoni9282@gmail.com>
2023-09-10 14:27:24 +0530

    final commit

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -3,8 +3,7 @@ import React from "react";
 export const useGetUserInfo = () => {
   // JSON parse is use to get  back data in object from string form, from local storage,
   // it like decrypt of JSON.stringify()
-  const { name, profilePhoto, userID, isAuth } = JSON.parse(
-    localStorage.getItem("auth")
-  );
+  const { name, profilePhoto, userID, isAuth } =
+    JSON.parse(localStorage.getItem("auth")) || {};
   return { name, profilePhoto, userID, isAuth };
 };
